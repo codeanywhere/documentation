a71eead0d9c9232992689821bf5ecd5a38c9738c
Fabjan Vučina <fabjanvucina@gmail.com>
2021-06-30 14:06:00 +0000

    Resolve "divide main sections"

--- src/index.ts
+++ src/index.ts
@@ -22,26 +22,43 @@ const getTree = async () =>
   JSON.parse(await fs.readFile('./sidebar.json', 'utf8'))
 
 //TODO: correct res type
-const render = async (res: any, category: string, slug: string) => {
+const render = async (
+  res: any,
+  section: string,
+  subsection: string,
+  article: string
+) => {
   res.set(
     'Cache-Control',
     'public, max-age=600, stale-if-error=60, stale-while-revalidate=60'
   )
   res.locals.rendered = await renderMarkdown(
-    path.join(__dirname, 'docs', 'markdowns', category, slug) + '.md'
+    path.join(__dirname, 'docs', 'markdowns', section, subsection, article) +
+      '.md'
   )
   const t = await getTree()
   res.locals.sidebar = t
-  res.locals.active = category + '/' + slug
+  res.locals.active = section + '/' + subsection + '/' + article
 
-  const cobj = t.find((c: { slug: string }) => c.slug === category)
-  const dobj = cobj.contents.find((d: { slug: string }) => d.slug === slug)
+  const sectionObj = t.find((c: { slug: string }) => c.slug === section)
+  const subsectionObj = sectionObj.contents.find(
+    (c: { slug: string }) => c.slug === subsection
+  )
+  const articleObj = subsectionObj.contents.find(
+    (d: { slug: string }) => d.slug === article
+  )
 
-  if (category === 'getting-started' && slug === 'what-is-codeanywhere') {
+  if (
+    section === 'general' &&
+    subsection === 'getting-started' &&
+    article === 'what-is-codeanywhere'
+  ) {
     // landing page gets a short title for search engine visibility
     res.locals.title = 'Codeanywhere Docs'
   } else {
-    res.locals.title = `Codeanywhere Docs - ${dobj ? dobj.name : slug}`
+    res.locals.title = `Codeanywhere Docs - ${
+      articleObj ? articleObj.name : article
+    }`
   }
   res.render('index.ejs')
 }
@@ -52,12 +69,12 @@ const renderMarkdown = async (path: string) => {
 }
 
 app.get('/', (req, res) => {
-  render(res, 'getting-started', 'what-is-codeanywhere')
+  render(res, 'general', 'getting-started', 'what-is-codeanywhere')
 })
 
-app.get('/:category/:slug', async (req, res) => {
-  const { category, slug } = req.params
-  render(res, category, slug).catch(err => {
+app.get('/:section/:subsection/:article', async (req, res) => {
+  const { section, subsection, article } = req.params
+  render(res, section, subsection, article).catch(err => {
     res.send(`
       <h1>Something went wrong!</h1>
 `)
